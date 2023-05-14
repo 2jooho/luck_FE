f192e34adc1cea4e26dd1274f625e3a667eda724
2jooho <tjrwn1003@gmail.com>
2023-05-15 01:44:19 +0900

    모달 수정

--- src/components/Modall.tsx
+++ src/components/Modall.tsx
@@ -1,80 +1,93 @@
-// // import Modal from 'react-native-simple-modal';
-// import React, { useState, useEffect, useRef } from 'react';
-// import {BackHandler ,Modal,View, Text, SafeAreaView, FlatList, ActivityIndicator, StyleSheet, StatusBar, Image, ImageBackground, TextInput, Alert, TouchableOpacity} from 'react-native';
+// import Modal from 'react-native-simple-modal';
+import React, { useState, useEffect, useRef } from 'react';
+import {BackHandler ,Modal,View, Text, SafeAreaView, FlatList, ActivityIndicator, StyleSheet, StatusBar, Image, ImageBackground, TextInput, Alert, TouchableOpacity} from 'react-native';
 
-// const Modall = ({openYn}) => {
-//     const [isModalVisible, setIsModalVisible] = useState(false);
-//     useEffect(()=>{
-//         setIsModalVisible(openYn);
-//     }, openYn)
+const Modall = ({openYn}: any) => {
+    const [isModalVisible, setIsModalVisible] = useState(openYn);
 
-//     return(
-//         <View style={styles.container}> //가장 큰 컨테이너
-//             <Modal //모달창
-//                 animationType={"none"} //slide, fade, none
-//                 transparent={true}
-//                 visible={isModalVisible}
-//                 onRequestClose={() => { // 뒤로가기 버튼(Android) 또는 메뉴버튼(Apple TV)을 선택할 때 실행할 함수
-//                     setIsModalVisible(false)
-//                     console.log("modal appearance")
-//                 }}
-//             >
-//                 <View> //모달창에서 보여줄 화면 꾸미기
-//                     <Text style={{fontSize: 20}}>모달창이요!</Text>
-//                     <Text style={{fontSize: 20}}>너무 어려워요!</Text>
-//                     <TouchableOpacity style={{margin: 3}} onPress={() => BackHandler.exitApp()}> //누르면 모달창을 닫아주는 버튼
-//                         <Text style={styles.text}>닫으시요</Text>
-//                     </TouchableOpacity>
-//                 </View>
-//             </Modal>
-//         </View>
-//     )
-//     // return(
-//     //
-//     //             <Modal //모달창
-//     //                 // offset={this.state.offset}
-//     //                 open={openYn} //상태가 오픈이어야함.
-//     //                 modalDidOpen={() => console.log('modal did open')} //모달이 열릴경우 콘솔창에 안내문을 띄운다.
-//     //                 modalDidClose={() => BackHandler.exitApp()} //모달창을 닫을 경우 앱 종료
-//     //                 style={{alignItems: 'center'}}>
-//     //                 <View> //모달창에서 보여줄 화면 꾸미기
-//     //                     <Text style={{fontSize: 20}}>모달창이요!</Text>
-//     //                     <Text style={{fontSize: 20}}>너무 어려워요!</Text>
-//     //                     <TouchableOpacity style={{margin: 3}} onPress={() => BackHandler.exitApp()}> //누르면 모달창을 닫아주는 버튼
-//     //                         <Text style={styles.text}>닫으시요</Text>
-//     //                     </TouchableOpacity>
-//     //                 </View>
-//     //             </Modal>
-//     //         </View>
-//     // )
+    return(
+        <View style={styles.container2}> 
+            <Modal //모달창
+                animationType={"none"} //slide, fade, none
+                transparent={true}
+                visible={isModalVisible}
+                onRequestClose={() => { // 뒤로가기 버튼(Android) 또는 메뉴버튼(Apple TV)을 선택할 때 실행할 함수
+                    setIsModalVisible(false)
+                    console.log("modal appearance")
+                }}
+            >
+                <View> 
+                    <Text style={{fontSize: 20}}>모달창이요!</Text>
+                    <Text style={{fontSize: 20}}>너무 어려워요!</Text>
+                    <TouchableOpacity style={{margin: 3}} onPress={() => BackHandler.exitApp()}> 
+                        <Text style={styles.text2}>닫으시요</Text>
+                    </TouchableOpacity>
+                </View>
+            </Modal>
+        </View>
+    )
+    // return(
+    //
+    //             <Modal //모달창
+    //                 // offset={this.state.offset}
+    //                 open={openYn} //상태가 오픈이어야함.
+    //                 modalDidOpen={() => console.log('modal did open')} //모달이 열릴경우 콘솔창에 안내문을 띄운다.
+    //                 modalDidClose={() => BackHandler.exitApp()} //모달창을 닫을 경우 앱 종료
+    //                 style={{alignItems: 'center'}}>
+    //                 <View> //모달창에서 보여줄 화면 꾸미기
+    //                     <Text style={{fontSize: 20}}>모달창이요!</Text>
+    //                     <Text style={{fontSize: 20}}>너무 어려워요!</Text>
+    //                     <TouchableOpacity style={{margin: 3}} onPress={() => BackHandler.exitApp()}> //누르면 모달창을 닫아주는 버튼
+    //                         <Text style={styles.text}>닫으시요</Text>
+    //                     </TouchableOpacity>
+    //                 </View>
+    //             </Modal>
+    //         </View>
+    // )
 
-// }
-// export default Modall;
+}
+export default Modall;
 
 
-// const styles = StyleSheet.create({
-//     container: {
-//         zIndex:3,
-//         position:'absolute',
-//         height:'100%',
-//         width:'100%',
-//         justifyContent:"center",
-//         alignContent:"center",
-//         alignItems:"center",
-//         paddingTop: 50
-//     },
-//     buttontext:{
+const styles = StyleSheet.create({
+    container2: {
+        zIndex:3,
+        position:'absolute',
+        height:'100%',
+        width:'100%',
+        justifyContent:"center",
+        alignContent:"center",
+        alignItems:"center",
+        paddingTop: 50
+    },
+    text2:{
+        position:'relative',
+        fontSize:15,
+        fontWeight:'700',
+        left:'40%',
+    },
+    container: {
+        zIndex:3,
+        position:'absolute',
+        height:'100%',
+        width:'100%',
+        justifyContent:"center",
+        alignContent:"center",
+        alignItems:"center",
+        paddingTop: 50
+    },
+    buttontext:{
 
-//         position:'relative',
-//         left:170,
-//         bottom:350,
-//         fontSize:20,
-//     },
-//     text:{
-//         position:'relative',
-//         fontSize:15,
-//         fontWeight:'700',
-//         left:'40%',
-//     }
+        position:'relative',
+        left:170,
+        bottom:350,
+        fontSize:20,
+    },
+    text:{
+        position:'relative',
+        fontSize:15,
+        fontWeight:'700',
+        left:'40%',
+    }
 
-// })
+})
--- src/screens/MainPage.tsx
+++ src/screens/MainPage.tsx
@@ -1,6 +1,6 @@
-import React, {useState, useCallback, useEffect} from 'react';
+import React, {useState, useCallback, useEffect, useRef} from 'react';
 import styled from 'styled-components/native';
-import {StatusBar, StyleSheet, TouchableOpacity, SafeAreaView, Text, View, Image, ScrollView, RefreshControl, Pressable, BackHandler, Alert} from 'react-native';
+import {PanResponder, Dimensions, StatusBar, StyleSheet, TouchableOpacity, SafeAreaView, Text, View, Image, ScrollView, RefreshControl, Pressable, BackHandler, Alert, Modal, TouchableWithoutFeedback, Animated} from 'react-native';
 import Header from '../components/Header';
 import MainHeader from '../components/MainHeader'
 import Colors from '../constants/Colors';
@@ -99,6 +99,39 @@ const MainPage = ({navigation, route}) => {
     const [refreshing, setRefreshing] = useState(false);
     const [userId, setUserId] = useState('');
     const [loading, setLoading] = useState(false);
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    const screenHeight = Dimensions.get('screen').height;
+    const panY = useRef(new Animated.Value(screenHeight)).current;
+    const translateY = panY.interpolate({
+        inputRange: [-1, 0, 1],
+        outputRange: [-1, 0, 1]
+    });
+    const resetBottomSheet = Animated.timing(panY, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: true
+    });
+    const panResponder = useRef(PanResponder.create({
+        onStartShouldSetPanResponder: () => true,
+        onMoveShouldSetPanResponder: () => true,
+        onPanResponderMove: (event, gestureState) => panY.setValue(gestureState.dy),
+        onPanResponderRelease: (event, gestureState) => {
+            if (gestureState.dy > 0 && gestureState.vy > 1.5) {
+                closeModal();
+            } else {
+                resetBottomSheet.start();
+            }
+        }
+    })).current;
+    const closeBottomSheet = Animated.timing(panY, {
+        toValue: screenHeight,
+        duration: 300,
+        useNativeDriver: true
+    });
+    const closeModal = () => {
+        closeBottomSheet.start(() => setIsModalVisible(false));
+    }
+
 
     //refreshcontrol을 호출할 때 실행되는 callback함수
     const onRefresh = useCallback(() => {
@@ -160,13 +193,50 @@ const MainPage = ({navigation, route}) => {
                     <CateListComponent navigation={navigation} cateDtoList={users.cateDtoList}></CateListComponent>
                 </View>
                 {/* 캘린더 이미지 */}
-                <Pressable onPress={() => <Modall isOpen = {true}></Modall>}><Image source={require('../assets/images/main/image-low-001.jpg')} style={styles.ScheduleImgView}></Image></Pressable>
+                <Pressable onPress={() => setIsModalVisible(!isModalVisible)}><Image source={require('../assets/images/main/image-low-001.jpg')} style={styles.ScheduleImgView}></Image></Pressable>
             </ScrollView>
+                <Modal //모달창
+                    animationType={"none"} //slide, fade, none
+                    transparent={true}
+                    visible={isModalVisible}
+                    onRequestClose={() => { // 뒤로가기 버튼(Android) 또는 메뉴버튼(Apple TV)을 선택할 때 실행할 함수
+                        setIsModalVisible(false)
+                    }}
+                >
+                    <Pressable 
+                    style={styles.modalOverlay}
+                    onPress={() => setIsModalVisible(!isModalVisible)}>   
+                        
+                        <View style={styles.bottomSheetContainer}>
+                                <Text style={modalInnerStyle.recipeTitle}>알림</Text>
+                                <Text style={[modalInnerStyle.coin]}>오픈 준비중 입니다.</Text>
+                                <Pressable onPress={()=> setIsModalVisible(false)}>
+                                    <View style={modalInnerStyle.btnView}>
+                                        <Text style={modalInnerStyle.btnText}>확인</Text>
+                                    </View>
+                                </Pressable>
+                        </View>
+                    </Pressable>
+                </Modal>
         </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
+    modalOverlay: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems:'center',
+        backgroundColor: 'rgba(0, 0, 0, 0.4)'
+    },  // 모달이 띄워졌을 때 화면을 어둡게 하기 위한 오버레이
+    bottomSheetContainer: {
+        width: wp(80),
+        height: hp(30),
+        backgroundColor: '#fff',
+        borderTopLeftRadius: 7,
+        borderTopRightRadius: 7,
+        padding: wp(7)
+    },  // 모달 스타일
     safeView: {
         flex: 1,
         bottom: 0
@@ -264,6 +334,34 @@ const styles = StyleSheet.create({
         
     },
 
+})
+
+const modalInnerStyle = StyleSheet.create({
+    recipeTitle: {
+        fontSize: wp(7),
+        fontWeight: '700'
+    },
+    coin: {
+        fontSize: wp(5),
+        fontWeight: '700',
+        paddingTop: hp(3),
+        textAlign: 'center'
+    },
+    btnView: {
+        width: wp(15),
+        height: hp(5),
+        backgroundColor: '#e6c4fc',
+        borderRadius: 5,
+        alignSelf:'center',
+        justifyContent:'center',
+        marginTop: hp(4)
+    },
+    btnText: {
+        fontSize: wp(4),
+        fontWeight: '700',
+        textAlign: 'center',
+        color: '#fff',
+    },
 })
 
 export default MainPage;
